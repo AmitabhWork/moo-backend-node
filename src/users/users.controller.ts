e8de3fc83a47e8af927cd51b842da53aaa916340
amitabh_macbook_pro <amitabhs@sathguru.com>
2021-07-13 22:48:10 +0530

    all user endpintes added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/')
+  getHashtags(): string {
+    // TODO: add code
+    return 'all top Users';
+  }
+  @Get('/@:username')
+  getUserByUsername(@Param() _param): string {
+    return `details of usernaem = ${_param.username}`;
+  }
+  @Get('/:userId')
+  getUserByUserId(@Param() _param): string {
+    return `details of userId = ${_param.userId}`;
+  }
+  @Post('/')
+  createNewUser(): string {
+    return 'new user created';
+  }
+
+  @Patch('/:userid')
+  updateUserDetails(@Param() param): string {
+    return `details of user (id = ${param.userid}) updated`;
+  }
+
+  @Put('/:userid/follow')
+  followUser(): string {
+    return 'you followed user';
+  }
+
+  @Delete('/:userid/follow')
+  unfollowUser(): string {
+    return 'you unfollowed user';
+  }
+
+  @Get('/:userid/followers')
+  getFollowersOfUser(): string {
+    return 'get followers of user';
+  }
+
+  @Put('/:userid/followees')
+  getFolloweesOfUser(): string {
+    return `get followees of given user`;
+  }
+}
